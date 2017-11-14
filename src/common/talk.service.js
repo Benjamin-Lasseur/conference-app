e8ce4e88a3fa131c7de0eb90e314250dda3ed66f
Benjamin Lasseur <benjamin.lasseur@gmail.com>
2017-11-14 15:50:07 +0100

    utilisation données externes

--- src/common/talk.service.js
+++ src/common/talk.service.js
@@ -1,19 +1,46 @@
 
 export default class TalkService {
 
+    constructor() {
+        this.speakers = []
+        this.sessions = []
+    }
+
     findAllSpeakers() {
-        return $.get("http://localhost:3000/speakers")    
+        return new Promise((resolve, reject) => {
+            if (this.speakers.length == 0) {
+                $.get("https://raw.githubusercontent.com/2017-d13/conference-data/master/speakers.json").then(data => {
+                    this.speakers = JSON.parse(data)
+                    resolve(JSON.parse(data))
+                })
+            } else {
+                resolve(this.speakers)
+            }
+        })
     }
 
-    findSpeakerById(id){
-        return $.get(`http://localhost:3000/speakers/${id}`)
+    findSpeakerById(id) {
+        return this.findAllSpeakers().then(speakers => {
+            return speakers.find(speaker => { return speaker.id == id })
+        })
     }
 
-    findAllSessions(){
-        return $.get("http://localhost:3000/sessions") 
+    findAllSessions() {
+        return new Promise((resolve, reject) => {
+            if (this.sessions.length == 0) {
+                $.get("https://raw.githubusercontent.com/2017-d13/conference-data/master/sessions.json").then(data => {
+                    this.sessions = JSON.parse(data)
+                    resolve(JSON.parse(data))
+                })
+            } else {
+                resolve(this.sessions)
+            }
+        })
     }
 
-    findSessionById(id){
-        return $.get(`http://localhost:3000/sessions/${id}`)
+    findSessionById(id) {
+        return this.findAllSessions().then(sessions => {
+            return sessions.find(session => { return session.id == id })
+        })
     }
 }
--- src/speakers/list/index.js
+++ src/speakers/list/index.js
@@ -18,24 +18,17 @@ export default class SpeakerList {
             $('#navSessions').removeClass().addClass("nav-item nav-link")
             $("#main-view").html(template)
             let str = template
-            speakers.sort(this.compare).forEach(speaker => {
+            speakers.forEach(speaker => {
                 str = str + this.newLine(speaker)
             })
             $(".list-group").html(str)
 
-            $(".list-group-item-action").mouseover(function () {
+            $(".list-group-item-action").mouseover(function(){
             })
         })
     }
     newLine(speaker) {
-        return `<a id="${speaker.id}" href="#speaker-detail/${speaker.id}" class="list-group-item list-group-item-action">${speaker.lastname} ${speaker.firstname}</a>`
-    }
-    compare(a, b) {
-        if (a.lastname < b.lastname)
-            return -1;
-        if (a.lastname > b.lastname)
-            return 1;
-        return 0;
+        return `<a id="${speaker.id}" href="#speaker-detail/${speaker.id}" class="list-group-item list-group-item-action">${speaker.firstname} ${speaker.lastname}</a>`
     }
 }
 
